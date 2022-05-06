6bffbfa29c75187df7dfebc2dbfc2c6aaf25fa31
Andreas Mora75 <70494818+andreasmora@users.noreply.github.com>
2022-05-06 09:59:28 -0300

    Add files via upload

---
+++ Ex001.java
@@ -0,0 +1,10 @@
+// Faca um programa que mostre na tela a mensagem
+// olà mundo.
+
+public class Ex001 {
+    public static void main(String[] args) {
+
+        System.out.println("Ola Mundo!!!");
+    }
+
+}
